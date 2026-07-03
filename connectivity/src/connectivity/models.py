ccc62385002c4ef167ac21e61f13b1dfb0a0075a
MCMR <MCMR@gitee.com>
2026-07-03 20:22:38 +0800

    chore: add OPC UA configuration
    Register OPC UA adapter and simulator environment variables and add the asyncua dependency to the connectivity and analytics packages.

--- connectivity/src/connectivity/models.py
+++ connectivity/src/connectivity/models.py
@@ -6,3 +6,6 @@ import os
 MQTT_BROKER_HOST: str = os.getenv("MQTT_BROKER_HOST", "localhost")
 MQTT_BROKER_PORT: int = int(os.getenv("MQTT_BROKER_PORT", "1883"))
 BACKEND_URL: str = os.getenv("BACKEND_URL", "http://localhost:8000")
+OPCUA_ENDPOINT: str = os.getenv("OPCUA_ENDPOINT", "opc.tcp://localhost:4840/")
+OPCUA_DEVICE_ID: str = os.getenv("OPCUA_DEVICE_ID", "sensor_hcsr04_01")
+OPCUA_DISTANCE_NODE_ID: str = os.getenv("OPCUA_DISTANCE_NODE_ID", "ns=2;s=distance")
